6fd2bfc8b579aa14c445aa2310d7afbfe71c5a5f
Zen <muhzaini30@gmail.com>
2022-07-04 12:15:57 +0800

    tes

---
+++ script/pack.ts
@@ -0,0 +1,8 @@
+import esbuild from "esbuild";
+
+esbuild.build({
+  entryPoints: ["src/gigit.ts"],
+  outdir: "dist",
+  minify: true,
+  platform: "node",
+});
---
+++ src/gigit.ts
@@ -0,0 +1,14 @@
+import { readFileSync, writeFileSync } from "fs";
+import { $ } from "zx";
+const { parse } = JSON;
+
+let fileGigit: any = readFileSync("gigit.json").toString();
+fileGigit = parse(fileGigit);
+
+async function jalankan(): Promise<void> {
+  for (let n in fileGigit) {
+    await $`cp .git/hooks/${n}.sample .git/hooks/${n}`;
+    writeFileSync(`.git/hooks/${n}`, fileGigit[n]);
+  }
+}
+jalankan();
